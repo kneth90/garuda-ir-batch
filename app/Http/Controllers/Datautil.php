2da68ec66361bc8c27872a87b2da5414bdcbd3df
kneth90 <kenneth.maynard90@gmail.com>
2019-03-26 12:09:35 +0700

    m

--- app/Http/Controllers/Datautil.php
+++ app/Http/Controllers/Datautil.php
@@ -86,7 +86,13 @@ class Datautil extends BaseController
     private function set_report_selling(){
         echo "Te";
         if(isset($_POST['data'])) {
-            var_dump($_POST['data']);
+            $data = $_POST['data'];
+
+            foreach ($data as $v){
+                $this->db->table("report_selling")
+                        ->updateOrInsert(['tanggal' => $v->tanggal, 'costumer_id' => $v->costumer_id, 'product' => $v->product]
+                                        , ['sales_value' => $v->sales_value, 'sales_unit' => $v->sales_unit]);
+            }
 
         }
     }
